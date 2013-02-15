0e6fc6ebbf24559924e3948a345799751514cc79
ivetopz6@gmail.com <>
2013-02-15 09:21:36 +0000

    New commit folder Ivelina Velcheva

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -69,7 +69,8 @@ return array(
     ),
     'controllers' => array(
         'invokables' => array(
-            'Application\Controller\Index' => 'Application\Controller\IndexController'
+            'Application\Controller\Index' => 'Application\Controller\IndexController',
+            'Application\Controller\IvelinaVelcheva' => 'Application\Controller\IvelinaVelchevaController'
         ),
     ),
     'view_manager' => array(
